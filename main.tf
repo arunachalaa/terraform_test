da2125d83b0ad3af19dbe009ca80ca3516f27da5
arunachalaa <arun.achilus@gmail.com>
2024-02-21 07:29:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,8 @@ provider "aws" {
 resource "aws_instance" "ccit" {
   ami = "ami-0cf10cdf9fcd62d37"
   instance_type = "t2.micro"
+  count = 2
   tags = {
-    Name = "SampleTF_Instance_1"
+    Name = "SampleTF_Instance_test"
   }
 }
