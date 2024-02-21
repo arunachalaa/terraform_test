30505a6bae2055bef3ca7a58526124b510b255f1
arunachalaa <arun.achilus@gmail.com>
2024-02-21 07:18:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "sample" {
-  ami           = "ami-0cf10cdf9fcd62d37"
+resource "aws_instance" "ccit" {
+  ami = "ami-0cf10cdf9fcd62d37"
   instance_type = "t2.micro"
   tags = {
     Name = "SampleTF_Instance"
